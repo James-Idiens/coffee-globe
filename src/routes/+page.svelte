6575fa0b246df4ac0b1f88d6903ddd4980683234
James Idiens <jidiens@outlook.com>
2024-01-23 23:00:12 +1300

    Add animejs library and animation to the page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,140 @@
+<script>
+	import anime from 'animejs';
+
+	import { onMount } from 'svelte';
+
+	// Define a function to start the animation
+	const startAnimation = () => {
+		anime
+			.timeline({ loop: false })
+			.add({
+				targets: '.ml5 .line',
+				opacity: [0.5, 1],
+				scaleX: [0, 1],
+				easing: 'easeInOutExpo',
+				duration: 700
+			})
+			.add({
+				targets: '.ml5 .line',
+				duration: 600,
+				easing: 'easeOutExpo',
+				translateY: (el, i) => -0.625 + 0.625 * 2 * i + 'em'
+			})
+			.add({
+				targets: '.ml5 .ampersand',
+				opacity: [0, 1],
+				scaleY: [0.5, 1],
+				easing: 'easeOutExpo',
+				duration: 600,
+				offset: '-=600'
+			})
+			.add({
+				targets: '.ml5 .letters-left',
+				opacity: [0, 1],
+				translateX: ['0.5em', 0],
+				easing: 'easeOutExpo',
+				duration: 600,
+				offset: '-=300'
+			})
+			.add({
+				targets: '.ml5 .letters-right',
+				opacity: [0, 1],
+				translateX: ['-0.5em', 0],
+				easing: 'easeOutExpo',
+				duration: 600,
+				offset: '-=600'
+			})
+			.add({
+				targets: '.ml5',
+				opacity: 1,
+				duration: 1000,
+				easing: 'easeOutExpo',
+				delay: 1000
+			});
+	};
+
+	onMount(() => {
+		startAnimation();
+	});
+</script>
+
 <header
 	class="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img"
 >
-	<div class="p-5 text-2xl text-white bg-purple-300 bg-opacity-50 rounded-xl">
-		Welcome to my site!
+	<div class="p-5 text-white ml5">
+		<span class="text-wrapper">
+			<span class="line line1"></span>
+			<span class="letters letters-left">Around The World</span>
+			<span class="letters letters-right">In 80 Cups</span>
+			<span class="line line2"></span>
+		</span>
 	</div>
 </header>
 
-<div class="max-w-lg m-auto">
-	<p class="mb-4">
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat a magna non varius.
-		Proin leo felis, euismod non porta eget, varius sit amet sapien. Maecenas in nulla at leo
-		convallis consectetur id a sapien. Nulla nec pulvinar nisi. Vivamus non facilisis lacus, et
-		volutpat libero. Nulla ac odio aliquam, accumsan arcu ut, lacinia est. Nulla eu sem elit. Fusce
-		nec laoreet sem, semper molestie libero.
-	</p>
-	<p class="mb-4">
-		Ut sagittis lacus consequat accumsan venenatis. Sed sollicitudin, lectus et fringilla ultrices,
-		dolor nisi scelerisque tortor, vel finibus magna massa non nunc. Phasellus massa quam, egestas a
-		nisl sed, porta volutpat metus. Nunc sed elit ac tellus tempor cursus. Suspendisse potenti.
-		Vestibulum varius rutrum nisl nec consequat. Suspendisse semper dignissim sem viverra semper.
-		Nulla porttitor, purus nec accumsan pharetra, nisi dolor condimentum ipsum, id consequat nulla
-		nunc in ligula.
-	</p>
-	<p class="mb-12">
-		Nulla pharetra lacinia nisi, vitae mollis tellus euismod id. Mauris porta dignissim hendrerit.
-		Cras id velit varius, fermentum lectus vitae, ultricies dolor. In bibendum rhoncus purus vel
-		rutrum. Nam vulputate imperdiet fringilla. Donec blandit libero massa. Suspendisse dictum diam
-		mauris, vitae fermentum dolor tincidunt in. Pellentesque sollicitudin venenatis dolor, vitae
-		scelerisque elit ultrices eu. Donec eget sodales risus, quis dignissim neque.
-	</p>
+<div class="max-w-lg m-auto h-screen">
+	<div class="block max-w-[18rem] rounded-lg bg-black-200">
+		<div class="p-6">
+			<p class="py-6 mb-4 font-bold text-4xl text-center leading-normal text-gradient-el-salvador">
+				El Salvador
+			</p>
+		</div>
+	</div>
 </div>
 
 <style>
+	.text-gradient-el-salvador {
+		background: linear-gradient(to right, #0b66c2, #ffffff, #0b66c2);
+		-webkit-background-clip: text;
+		color: transparent;
+	}
 	.custom-img {
 		background-image: url('./heroimage.webp');
+	}
+
+	.ml5 {
+		position: relative;
+		font-weight: 300;
+		font-size: 4.5em;
+		color: #ffffff;
+	}
+
+	.ml5 .text-wrapper {
+		position: relative;
+		display: inline-block;
+		padding-top: 0.1em;
+		padding-right: 0.05em;
+		padding-bottom: 0.15em;
+		line-height: 1em;
+	}
+
+	.ml5 .line {
+		position: absolute;
+		left: 0;
+		top: 0;
+		bottom: 0;
+		margin: auto;
+		height: 3px;
+		width: 100%;
+		background-color: #ffffff;
+		transform-origin: 0.5 0;
+	}
+	.ml5 .letters {
+		display: inline-block;
+		opacity: 0;
+	}
+
+	@media (max-width: 768px) {
+		.ml5 {
+			font-size: 2.5em;
+			text-align: center;
+		}
+
+		.ml5 .line {
+			height: 0px;
+		}
+		.ml5 .text-wrapper {
+			display: block;
+			text-align: center;
+		}
 	}
 </style>
