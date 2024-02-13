49839da141c4e598f9544f3a97f1bea7c6ec678e
James Idiens <jidiens@outlook.com>
2024-02-13 19:34:05 +1300

    Update coffee image and layout

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -29,19 +29,39 @@
 	});
 </script>
 
-<div class="bg-black-100 h-screen overflow-scroll">
+<div
+	class="bg-black-100 min-h-screen overflow-y-auto flex flex-col md:flex-row justify-center items-center relative"
+>
 	{#if coffee}
-		<div class="text-white">
-			<h1 class="text-2xl">Country: {coffee.country}</h1>
-			<p>Region: {coffee.region}</p>
-			<p>Producer: {coffee.producer}</p>
-			<p>Process: {coffee.process}</p>
-			<p>Variety: {coffee.variety}</p>
-			<p>Tasting Notes: {coffee.tastingNotes}</p>
-			<p>Review: {coffee.review}</p>
+		<div class="flex-1 order-2 md:order-1 ml-2">
+			<img
+				src={coffee.image}
+				alt="Coffee"
+				class="w-full h-auto md:h-64 object-cover object-center"
+			/>
+		</div>
+		<div
+			class="flex-1 max-w-md mx-auto bg-gray-800 rounded-lg shadow-lg overflow-hidden order-1 md:order-2 md:max-w-1/3 md:mx-4 mt-4 md:mt-0"
+		>
+			<div class="p-6">
+				<h1 class="text-3xl font-bold text-white mb-4">Country: {coffee.country}</h1>
+				<p class="text-gray-400">Region: {coffee.region}</p>
+				<p class="text-gray-400">Producer: {coffee.producer}</p>
+				<p class="text-gray-400">Process: {coffee.process}</p>
+				<p class="text-gray-400">Variety: {coffee.variety}</p>
+				<p class="text-gray-400">Tasting Notes: {coffee.tastingNotes.join(', ')}</p>
+				<p class="text-gray-400">Review: {coffee.review}</p>
+			</div>
+		</div>
+		<div class="flex-1 order-3 md:order-3">
+			<!-- Placeholder for map -->
 		</div>
 	{:else}
 		<p class="text-white">Country not found!</p>
 	{/if}
-	<a href="/" class="rounded-full py-2 px-8 bg-gradient-to-l from-sky-400 to-blue-500">Back</a>
+	<div class="absolute bottom-1 w-full flex justify-center">
+		<a href="/" class="rounded-full py-2 px-8 bg-gradient-to-l from-sky-400 to-blue-500 mt-8"
+			>Back</a
+		>
+	</div>
 </div>
